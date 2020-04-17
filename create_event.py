d89e590dc298819a7837fcbfb684a03ba9dcd40a
Sayantan Biswas <sayantanbiswas10000@gmail.com>
2020-04-17 20:18:58 +0530

    Add files via upload

---
+++ create_event.py
@@ -0,0 +1,69 @@
+
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import datefinder
+from datetime import datetime, timedelta
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+timezone = 'Asia/Kolkata'
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+else:
+    flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
+    creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+service = build("calendar", "v3", credentials=creds)
+def create_events(start_time_str, summary, duration=1,attendees=None, description=None, location=None):
+    matches = list(datefinder.find_dates(start_time_str))
+    if len(matches):
+        start_time = matches[0]
+        end_time = start_time + timedelta(hours=duration)
+
+    event = {
+        'summary': summary,
+        'location': location,
+        'description': description,
+        'start': {
+            'dateTime': start_time.strftime("%Y-%m-%dT%H:%M:%S"),
+            'timeZone': timezone,
+        },
+        'end': {
+            'dateTime': end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+            'timeZone': timezone,
+        },
+
+
+        'attendees': [
+        {'email':attendees },
+    ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 15},
+            ],
+        },
+    }
+    return service.events().insert(calendarId='primary', body=event,sendNotifications=True).execute()
+create_events('10 April 1.00pm', 'Deloitte Start time',0.5,'sayantanbiswas1002@gmail.com')
+#Recurrence -'recurrence': [
+#    'RRULE:FREQ=WEEKLY;UNTIL=20200615T240000Z',
+#  ],
